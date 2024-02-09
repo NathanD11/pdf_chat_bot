ba405fb8a09a848594b542d0c6b54e6771683dbd
NathanD11 <115487142+NathanD11@users.noreply.github.com>
2024-02-09 10:39:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -19,9 +19,9 @@ def handle_userinput(user_question):
 
     for i, message in enumerate(st.session_state.chat_history):
         if i % 2 == 0:
-            st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+            st.write(message.content)
         else:
-            st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+            st.write(message.content)
 
 # Function to get PDF text
 def get_pdf_text(pdf_docs):
@@ -40,13 +40,13 @@ def get_text_chunks(text):
 
 # Function to create vector store
 def get_vectorstore(text_chunks):
-    embeddings = OpenAIEmbeddings(api_key="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")
+    embeddings = OpenAIEmbeddings(api_key="")
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
 
 # Function to create conversation chain
 def get_conversation_chain(vectorstore):
-    llm = ChatOpenAI(api_key="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")
+    llm = ChatOpenAI(api_key="")
     memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
     conversation_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=vectorstore.as_retriever(), memory=memory)
     return conversation_chain
